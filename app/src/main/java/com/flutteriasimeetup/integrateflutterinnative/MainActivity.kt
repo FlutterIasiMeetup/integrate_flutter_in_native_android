bbc09e3c3d6d4ceebab6a7c879d7a1adc2d15c93
Alex Terente <alex.terente@gmail.com>
2024-01-10 10:14:42 +0200

    Android Host App codebase

---
+++ app/src/main/java/com/flutteriasimeetup/integrateflutterinnative/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.flutteriasimeetup.integrateflutterinnative
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.flutteriasimeetup.integrateflutterinnative.ui.theme.IntegrateFlutterInNativeTheme
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntegrateFlutterInNativeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Greeting("Flutter")
+
+                        Button(
+                            onClick = {
+                                startActivity(
+                                    FlutterActivity.createDefaultIntent(this@MainActivity)
+                                )
+                            },
+                            modifier = Modifier
+                                .height(40.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text("Go to Flutter")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IntegrateFlutterInNativeTheme {
+        Greeting("Android")
+    }
+}
