34da2bb7003d34c85d1496be024847588b423032
Alex Terente <alex.terente@gmail.com>
2024-01-10 14:40:55 +0200

    implement method channels

--- app/src/main/java/com/flutteriasimeetup/integrateflutterinnative/MainActivity.kt
+++ app/src/main/java/com/flutteriasimeetup/integrateflutterinnative/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.flutteriasimeetup.integrateflutterinnative
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,10 +22,56 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.flutteriasimeetup.integrateflutterinnative.ui.theme.IntegrateFlutterInNativeTheme
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
+
+const val SINGLE_FLUTTER_ENGINE_ID = "com.integrateFlutterInNative.engineID"
 class MainActivity : ComponentActivity() {
+
+    private lateinit var flutterEngine: FlutterEngine
+    private lateinit var methodChannel: MethodChannel
+    private fun setUpFlutterEngine() {
+        flutterEngine = FlutterEngine(this)
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine
+        FlutterEngineCache
+            .getInstance()
+            .put(SINGLE_FLUTTER_ENGINE_ID, flutterEngine)
+
+        methodChannel = MethodChannel(flutterEngine.dartExecutor, "com.integrateFlutterInNative.channel")
+        methodChannel.setMethodCallHandler { call, result -> handleHostChannelMethods(call, result) }
+    }
+    private fun handleHostChannelMethods(
+        call: MethodCall,
+        result: MethodChannel.Result
+    ) {
+        when (call.method) {
+            "getToken" -> getToken(result)
+            "dismiss" -> dismiss(result)
+            else -> result.notImplemented()
+        }
+    }
+    private fun getToken(result: MethodChannel.Result) {
+        return result.success("f2722e1bf7faebdd5e63810b446bf499")
+    }
+    private fun dismiss(result: MethodChannel.Result) {
+        return result.success(true)
+    }
+    private fun incrementCounter() {
+        methodChannel.invokeMethod("incrementCounter",null)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpFlutterEngine()
+
         setContent {
             IntegrateFlutterInNativeTheme {
                 // A surface container using the 'background' color from the theme
@@ -44,7 +91,8 @@ class MainActivity : ComponentActivity() {
                         Button(
                             onClick = {
                                 startActivity(
-                                    FlutterActivity.createDefaultIntent(this@MainActivity)
+                                    FlutterActivity.withCachedEngine(SINGLE_FLUTTER_ENGINE_ID)
+                                        .build(this@MainActivity)
                                 )
                             },
                             modifier = Modifier
@@ -54,7 +102,19 @@ class MainActivity : ComponentActivity() {
                             Text("Go to Flutter")
                         }
 
-                        Spacer(modifier = Modifier.height(16.dp))
+                        // New Increment Counter Button
+                        Button(
+                            onClick = {
+                                incrementCounter()
+                            },
+                            modifier = Modifier
+                                .height(40.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text("Increment Counter")
+                        }
+
+                        Spacer(modifier = Modifier.height(40.dp))
                     }
                 }
             }
